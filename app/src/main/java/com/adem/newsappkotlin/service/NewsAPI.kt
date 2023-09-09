d0f06ef32af263ec52863740d9e609769514568f
Adem Muslugil <Adem.muslugil@hotmail.com>
2023-09-09 15:06:57 +0300

    security update

--- app/src/main/java/com/adem/newsappkotlin/service/NewsAPI.kt
+++ app/src/main/java/com/adem/newsappkotlin/service/NewsAPI.kt
@@ -1,16 +1,16 @@
 package com.adem.newsappkotlin.service
 
+import com.adem.newsappkotlin.Util.API_KEY
 import com.adem.newsappkotlin.model.Article
 import com.adem.newsappkotlin.model.NewsResponse
 import io.reactivex.Completable
 import io.reactivex.Observable
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsAPI {
-    //https://newsapi.org/v2/top-headlines?country=us&apiKey=5ef8fdd9f3f94529a4f6f2fdd858e6e6
-
-    @GET("top-headlines?country=us&apiKey=5ef8fdd9f3f94529a4f6f2fdd858e6e6")
-    fun getNews() : Single<NewsResponse>
+    @GET("top-headlines?country=us&apiKey=")
+    fun getNews(@Query("apiKey") apiKey: String = API_KEY) : Single<NewsResponse>
 
 }
